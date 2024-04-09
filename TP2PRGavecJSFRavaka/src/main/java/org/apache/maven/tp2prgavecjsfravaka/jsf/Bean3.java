36c50f29d8496806d0727e5524a296688fbbcc35
Ravaka82 <89504880+Ravaka82@users.noreply.github.com>
2024-04-09 12:17:17 +0300

    TP4 Template

---
+++ TP2PRGavecJSFRavaka/src/main/java/org/apache/maven/tp2prgavecjsfravaka/jsf/Bean3.java
@@ -0,0 +1,36 @@
+package org.apache.maven.tp2prgavecjsfravaka.jsf;
+
+import java.util.ArrayList;
+import java.util.List;
+import jakarta.inject.Named;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.faces.context.FacesContext;
+import jakarta.servlet.http.HttpServletRequest;
+
+@Named(value = "bean3")
+@RequestScoped
+public class Bean3 {
+
+   private int nombre;
+
+    public int getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(int nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Integer> getNombresSuivants() {
+        int nb = 5;
+        List<Integer> l = new ArrayList<>(nb);
+        for (int i = nombre; i < nombre + nb; i++) {
+            l.add(i);
+        }
+        return l;
+    }
+
+    public String afficher() {
+        return "affichage__4?nb=" + nombre + "&amp;faces-redirect=true";
+    }
+}
